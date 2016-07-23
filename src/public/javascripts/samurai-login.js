846de1b932bdd4425b78f3bce076d3c97ced0e7c
Rolf Gjeldnes <vyprolf@gmail.com>
2016-07-23 15:30:57 +0200

    Feedback when logging in

--- src/public/javascripts/samurai-login.js
+++ src/public/javascripts/samurai-login.js
@@ -2,16 +2,29 @@ var loginModule = angular.module('samurai', []);
 var loginController = loginModule.controller('samurai-login', ['$scope', '$http', function($scope, $http){
     $scope.username = '';
     $scope.password = '';
+    $scope.feedback = '';
+    $scope.loginSuccess = false;
+
+    function setFeedback(msg){
+        $scope.feedback = msg;
+    }
 
     $scope.login = function(){
+        setFeedback('Logging in...');
+        $scope.loginSuccess = false;
+
         let url = '/api/login';
         let payload = {user: $scope.username, pass: $scope.password};
         let config = { withCredentials: true };
-        $http.post(url, payload, config).then(r => {
-            let data = r.data;
-            console.log(data);
-            //TODO - login and redirect
-        });
+        $http.post(url, payload, config)
+            .then(r => {
+                if( r.status == 200 )
+                    $scope.loginSuccess = true;
+                setFeedback(r.data.message);
+            })
+            .catch( err => {
+                setFeedback('Error caught:' + err.data.message);
+            });
 
         $scope.password = '';
     }
