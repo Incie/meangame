86f97b45b85789407a63d25877748a6693e0d7d2
Rolf Gjeldnes <vyprolf@gmail.com>
2016-08-23 21:45:35 +0200

    improved session config

--- src/app.js
+++ src/app.js
@@ -1,26 +1,36 @@
-var express = require('express');
-var path = require('path');
-var morgan = require('morgan');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let morgan = require('morgan');
+let cookieParser = require('cookie-parser');
+let session = require('express-session');
+let bodyParser = require('body-parser');
 
-var apiRouter = require('./route/api.js');
+let apiRouter = require('./route/api.js');
 
-var app = express();
+let app = express();
 
 app.use(morgan(':date[iso] - (HTTP :http-version :status :method) [ip] :remote-addr [time] :response-time[3] ms [response-size] :res[content-length] [url] :url'));
 
 app.use(bodyParser.json({limit: '2mb'}));
 app.use(bodyParser.urlencoded({ extended: false, limit: '2mb'}));
 app.use(cookieParser());
-app.use( session({
-    secret: "privatStatiskIngentingHovedStrengArgumenter",
-    resave: true,
+
+let sessionConfig = {
+    secret: process.env.SECRET,
+    resave: false,
+    proxy: true,
     maxAge: 2 * 60 * 60 * 1000,
-    saveUninitialized: true,
-    cookie: { secure: false }
-}));
+    saveUninitialized: false,
+    cookie: { secure: true }
+};
+
+if( process.env.ENVIRONMENT === "DEVELOPMENT" ){
+    console.log('Setting DEVELOPMENT session configs');
+    sessionConfig.proxy = false;
+    sessionConfig.cookie.secure = false;
+}
+
+app.use(session(sessionConfig));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -50,7 +60,7 @@ app.all('*', function(req, res){
     res.status(404).send({message:'The hamster did not find this route in the registry'});
 });
 
-var port = (process.env.PORT || 3001);
+const port = (process.env.PORT || 3001);
 app.listen(port, function() {
     console.log('Node Server ('+process.version+')');
     console.log('Listening on '+port);
