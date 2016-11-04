175fafb67aa2ab4c91a58e57985ead1c1ca756da
Incie <vyprolf@gmail.com>
2016-11-04 12:18:07 +0100

    added a real-ip logging flag to show the actual request host in nodeconsole using nginx proxy

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ let apiRouter = require('./route/api.js');
 
 let app = express();
 
-app.use(morgan(':date[iso] - (HTTP :http-version :status :method) [ip] :remote-addr [time] :response-time[3] ms [response-size] :res[content-length] [url] :url'));
+app.use(morgan(':date[iso] - (HTTP :http-version :status :method) [ip] :real-ip [time] :response-time[3] ms [response-size] :res[content-length] [url] :url'));
 
 app.use(bodyParser.json({limit: '2mb'}));
 app.use(bodyParser.urlencoded({ extended: false, limit: '2mb'}));
@@ -29,6 +29,9 @@ if( process.env.ENVIRONMENT === "DEVELOPMENT" ){
     console.log('Setting DEVELOPMENT session configs');
     sessionConfig.proxy = false;
     sessionConfig.cookie.secure = false;
+    morgan.token('real-ip', function(req, res) { return req.connection.remoteAddress; });
+} else {
+    morgan.token('real-ip', function(req, res) { return req.headers['x-real-ip']; });
 }
 
 app.use(session(sessionConfig));
